dd32e974d4878cdffcabee8a492988cca014f4fa
Forhad5194 <forhadhossain5194@gmail.com>
2024-01-20 17:02:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="avatar online">
+        <div className="w-24 rounded-full">
+          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <div className="avatar offline">
+        <div className="w-24 rounded-full">
+          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Shared/NavBar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+
+    const navlist = <>
+     <NavLink>HOME </NavLink>
+     <NavLink>CONTACT US  </NavLink>
+     <NavLink>DASHBOARD</NavLink>
+     <NavLink>OUR MENU</NavLink>
+     <NavLink>OUR SHOP</NavLink>
+     
+    </>
+
+
+
+
+
+
+    return (
+        <div className="navbar fixed z-30 bg-opacity-10 max-w-screen-xl mx-auto bg-black">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                      {navlist}
+                    </ul>
+                </div>
+                <div className="cursor-pointer text-white">
+                    <h2 className="font-black text-xl">BISTRO BOSS </h2>
+                    <p className=" font-bold text-xl">Restaurant</p>
+                </div>
+            </div>
+            <div className="navbar-end  hidden lg:flex">
+                <ul className="menu menu-horizontal text-white  text-xs font-normal gap-5 px-1">
+                    {navlist}
+                </ul>
+            </div>
+           
+        </div>
+    );
+};
+
+export default Navbar;
